e163f3168622d3c5a9c3f9241e4b9367f1d4912c
koichi <zekouhongyi@gmail.com>
2021-07-03 10:18:55 +0000

    upload

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -78,21 +78,21 @@ public function edit(Request $request)
       // News Modelからデータを取得する
       $news = News::find($request->id);
       // 送信されてきたフォームデータを格納する
-      $news_form = $request->all();
+      $form = $request->all();
       if ($request->remove == 'true') {
           $news_form['image_path'] = null;
       } elseif ($request->file('image')) {
-         $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
-         $news->image_path = Storage::disk('s3')->url($path);
+          $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+          $news->image_path = Storage::disk('s3')->url($path);
       } else {
           $news_form['image_path'] = $news->image_path;
       }
 
-      unset($news_form['image']);
-      unset($news_form['remove']);
-      unset($news_form['_token']);
+      unset($form['image']);
+      unset($form['remove']);
+      unset($form['_token']);
       // 該当するデータを上書きして保存する
-      $news->fill($news_form)->save();
+      $news->fill($form)->save();
       
       $history = new History;
       $history->news_id = $news->id;
